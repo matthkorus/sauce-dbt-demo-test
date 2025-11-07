34428f7861ae875f5e75b845ed119a14983bbd63
matt korus (matt+sldbt@etleap.com) <matt+sldbt@etleap.com>
2025-11-07 16:44:21 +0000

    Deleted model_block_test, order_info, order_info_copy and order_info_iceberg.

--- models/order_info_iceberg.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        materialized='table',
-        table_format = 'iceberg',
-        external_volume = 'MK_ICEBERG_EXTERNAL_VOLUME'
-    )
-}}
-
-select * from {{ ref('order_info') }}
