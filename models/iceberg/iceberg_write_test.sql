8822fbca7dea04f55161883b8cabc4273b5a5a9a
matthkorus <130331198+matthkorus@users.noreply.github.com>
2026-05-01 11:22:29 -0500

    make models work to test 1.11 CI

--- models/iceberg/iceberg_write_test.sql
+++ models/iceberg/iceberg_write_test.sql
@@ -1,12 +1,8 @@
-{{
-    config(
-        materialized='incremental',
-        catalog_name='snowflake_cld'
-    )
-}}
 
 select * exclude (audit_id, author_id, id, ticket_id)
 from {{ source('demo','TICKET_COMMENTS') }} tc
+/* 
 {% if is_incremental() %}
     where tc.created_at > (select max(created_at) from {{ this }})
 {% endif %}
+*/
