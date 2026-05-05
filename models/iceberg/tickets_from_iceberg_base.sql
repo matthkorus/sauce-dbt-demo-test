44e111735e55f40b045d7857868233f2609aab3c
matthkorus <130331198+matthkorus@users.noreply.github.com>
2026-05-05 06:53:33 -0500

    remove select exclusion, update create table macro

--- models/iceberg/tickets_from_iceberg_base.sql
+++ models/iceberg/tickets_from_iceberg_base.sql
@@ -1,11 +1,7 @@
+{{ config(
+    materialized='table',
+    table_format='iceberg'
+) }}
 
-{{
-    config(
-        materialized='table',
-        catalog_name='snowflake_cld',
-        contract={'enforced': true}
-    )
-}}
-
-select * exclude (brand_id, assignee_id,group_id,requester_id,organization_id,submitter_id)
-from {{ source('demo','ZENDESK_TICKETS') }}
+select *
+from TEST_ETLEAP.MK_DEMO.ZENDESK_TICKETS
