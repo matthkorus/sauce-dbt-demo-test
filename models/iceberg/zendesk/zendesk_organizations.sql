a7faafb8c41aef94b8daa8218fe7aeabea68488f
matthkorus <130331198+matthkorus@users.noreply.github.com>
2026-05-11 13:38:29 -0500

    reset tests

--- models/iceberg/zendesk/zendesk_organizations.sql
+++ models/iceberg/zendesk/zendesk_organizations.sql
@@ -1,11 +1,7 @@
 {{ config(
-    materialized='incremental',
+    materialized='table',
     unique_key='id'
 ) }}
 
 select *
 from {{ source('demo','ORGANIZATIONS') }}
-
-{% if is_incremental() %}
-where updated_at > (select max(updated_at) from {{ this }})
-{% endif %}
--- models/iceberg/zendesk/zendesk_users.sql
+++ models/iceberg/zendesk/zendesk_users.sql
@@ -1,11 +1,14 @@
 {{ config(
     materialized='incremental',
-    unique_key='id'
+    unique_key='id',
+    schema='zendesk'
 ) }}
+
+{% set relation = adapter.get_relation(this.database, this.schema, this.table) %}
+{{ log("type=" ~ relation.type ~ ", database=" ~ relation.database ~ ", schema=" ~ relation.schema ~ ", identifier=" ~ relation.identifier, info=true) }}
 
 select *
 from {{ source('demo','ZENDESK_USERS') }}
-
 {% if is_incremental() %}
 where updated_at > (select max(updated_at) from {{ this }})
 {% endif %}
